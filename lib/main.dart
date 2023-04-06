709aa1a599fc7a3830c9962ed6478894957168c5
Nimantha Karunanayake <nimanthakmp@gmail.com>
2023-04-07 03:00:24 +0530

    Randomly Generate the Dice
    Randomly generate the dice using random variable with state full widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
-    addMaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.purple,
@@ -22,9 +23,16 @@ void main() {
   );
 }
 
-class DiceApp extends StatelessWidget {
+class DiceApp extends StatefulWidget {
   const DiceApp({Key? key}) : super(key: key);
 
+  @override
+  State<DiceApp> createState() => _DiceAppState();
+}
+
+class _DiceAppState extends State<DiceApp> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -32,21 +40,28 @@ class DiceApp extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: TextButton(
-              onPressed: (){
-               print('Left button pressed');
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                });
               },
               child: Image.asset(
-                'images/dice1.png',
+                'images/dice$leftDiceNumber.png',
               ),
             ),
           ),
+
+
           Expanded(
             child: TextButton(
-              onPressed: (){
-                print('Right side clicked');
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
+
               },
               child: Image.asset(
-                'images/dice1.png',
+                'images/dice$rightDiceNumber.png',
               ),
             ),
           ),
