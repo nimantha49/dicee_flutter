cd9f7f77be14e6bc112ce8010123420f730f3c5d
Nimantha Karunanayake <nimanthakmp@gmail.com>
2023-05-16 23:57:36 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,6 @@ void main() {
           centerTitle: true,
           title: const Text(
             'Dicee',
-            // style: TextStyle(
-            //   fontFamily: 'Pacifico',
-            // ),
           ),
           backgroundColor: Colors.purple,
         ),
